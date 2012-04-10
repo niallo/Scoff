5c89e7d6bb4ca856fd19714b2c5819249e7f8403
Niall O'Higgins <niallo@unworkable.org>
2012-04-09 21:20:26 -0700

    reformat

--- test/test.js
+++ test/test.js
@@ -2,10 +2,9 @@ var scoff = require('../index.js');
 
 describe('Scoff', function() {
     it ('should initialize correctly', function() {
-      var t = scoff.wrap(__dirname + '/lib/test_account_database',
-        {requires:
-            {'somedb':
-            {
+      var t = scoff.wrap(__dirname + '/lib/test_account_database', {
+          requires: {
+            'somedb': {
             }
         }
       });
